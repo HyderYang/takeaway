3f9b8d09e95513185afc5a5c5fd4f46a0cb3e6da
hyder <yanghuan0613@gmail.com>
2018-02-23 18:12:23 +0800

    商品内容侧边栏布局完成

---
+++ src/components/sell/goods/goods-food.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="goods">
+    <div class="foods-wrapper">
+
+    </div>
+	</div>
+</template>
+
+<script>
+	/**
+	 * 组件概述:
+	 *
+	 * 使用方法:
+	 *
+	 *
+	 * author:
+	 *
+	 * 时间   :
+	 */
+	export default {
+		name: "goods-food",
+    props: {
+		  goods: {
+		    type: Object
+      }
+    }
+	}
+</script>
+
+<style scoped lang="less">
+  .goods {
+    position : absolute;
+    width    : 100%;
+    top      : 174px;
+    bottom   : 46px;
+    display  : flex;
+    overflow : hidden;
+
+    .foods-wrapper {
+      float : 1;
+    }
+  }
+</style>
